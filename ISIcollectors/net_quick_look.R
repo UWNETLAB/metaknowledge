84edd2acf5f0eb60b11c9d781888d67fa22fb4c8
John McLevey <john.mclevey@uwaterloo.ca>
2015-04-15 07:57:12 -0500

    write new graphml for visone

--- ISIcollectors/net_quick_look.R
+++ ISIcollectors/net_quick_look.R
@@ -25,7 +25,7 @@ length(V(net)) # no. of vertices
 print("no. of edges:")
 length(E(net)) # no. of edges
 
-# test for power law distribution 
+  # test for power law distribution 
 pdf("log_log_deg_dist.pdf")
 plot(degree.distribution(net, cumulative = TRUE), 
      log="xy", xlab = "Degree", ylab = "Cumulative Probability",
@@ -56,28 +56,28 @@ clique.number(net)
 
 # centralities 
 
-# normalized degree centrality
+    # normalized degree centrality
 n <- vcount(net)
 degree.norm <- degree(net)/(n-1)
 
-# betweenness centrality
+    # betweenness centrality
 betweennesscent <- betweenness(net)
 names(betweennesscent) <- V(net)$name
 ind <- order(-betweennesscent)
 # betweennesscent[ind][1:20] # show top 20 betweenness scores
 
-# normalized betweenness centrality
+    # normalized betweenness centrality
 n <- vcount(net)
 betweenness.norm <- betweenness(net)/(0.5*(n-1)*(n-2))  # undirected
 ind <- order(-betweenness.norm)
 # betweenness.norm[ind][1:20]
 
-# eigenvector centrality
+    # eigenvector centrality
 eigenvectorcent <- igraph::evcent(net)$vector
 ind <- order(-eigenvectorcent)
 # eigenvectorcent[ind][1:20] # show top 20 eigenvector scores
 
-# write out 
+    # write out 
 net_node_stats <- cbind(V(net)$id, degree, degree.norm, betweenness.norm, eigenvectorcent)
 write.csv(net_node_stats, "net_node_stats.csv")
 
@@ -91,26 +91,47 @@ V(net)$eigenvector.R <- igraph::evcent(net)$vector
 # fast and greedy community detection 
 
 fg <- fastgreedy.community(simplify(as.undirected(net)))
-print("Fast and Greedy Community Detection:")
-fg
+print("fast and greedy community detection, number of communities:")
+length(fg)
 
 memb <- community.to.membership(net, fg$merges, steps=which.max(fg$modularity)-1)
 #memb
 
-colbar <- rainbow(memb$membership+20)
+#plot(fg, net, vertex.label = NA)
+
+colbar <- rainbow(length(fg))
 col <- colbar[memb$membership+1]
 
+# walk trap community detection
+
+wc <- walktrap.community(net)
+# modularity(wc)
+# membership(wc)
 
 # quick plot
 
 print("time for fg layout")
-system.time(lay <- layout.fruchterman.reingold(net, niter=1000, coolexp=0.5,
-                                               area=vcount(net)^2.3,
-                                               repulserad=vcount(net)^2.8))
 
-pdf(file = "net_quickplot.pdf")
+#lay <- layout.fruchterman.reingold(net, niter=1000, coolexp=0.5,
+#                                   area=vcount(net)^2.3,
+#                                   repulserad=vcount(net)^2.8)
+
+lay <- layout.fruchterman.reingold(net, niter=1000, coolexp=0.5,
+                                   area=vcount(net)^2.3,
+                                   repulserad=vcount(net)^2.8)
+
+pdf(file="net_walktrap.pdf")
+plot(net, layout = lay, vertex.size = 2, edge.width = .5, 
+     vertex.label = NA, vertex.color = membership(wc), vertex.frame.color = NA)
+dev.off()
+
+pdf(file = "net_fastgreedy.pdf")
 set.seed(3952)
 par(mar=c(0,0,0,0)+.1) # reduce extra whitespace
-plot(net, layout = lay, vertex.size = 3, edge.width = 1, 
+plot(net, layout = lay, vertex.size = 2, edge.width = .5, 
      vertex.label = NA, vertex.color = col, vertex.frame.color = NA)
 dev.off()
+
+# for visone 
+
+write.graph(net, format="graphml", file="co-CiteNetwork.graphml")

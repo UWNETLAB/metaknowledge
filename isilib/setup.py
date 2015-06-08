484aa3f4f3a82290644827d01a70f3f6c77ade75
Reid McIlroy-Young <reid@reidmcy.com>
2015-06-08 16:23:21 -0400

    Started command line interface

--- isilib/setup.py
+++ isilib/setup.py
@@ -6,7 +6,7 @@ setup(name='isilib',
     keywords= 'isi wos',
     install_requires= ['networkx'],
     packages=['isilib'],
-    scripts=['bin/isilib-test', 'bin/isilib-coauthmaker', 'bin/isilib-cocitemaker', 'bin/isilib-makeListofcitations', 'bin/isilib-citemaker', 'bin/isilib-collectRecords', 'bin/isilib-cociteAuthsMaker'],
+    scripts=['bin/isilib-test', 'bin/isilib-coauthmaker', 'bin/isilib-cocitemaker', 'bin/isilib-makeListofcitations', 'bin/isilib-citemaker', 'bin/isilib-collectRecords', 'bin/isilib-cociteAuthsMaker', 'bin/isilib-CLI'],
     test_suite='tests',
     #data_files= [('/tests', ["testFile.isi", "OnePaper.isi", "ManyAuthors.isi"])]
 )

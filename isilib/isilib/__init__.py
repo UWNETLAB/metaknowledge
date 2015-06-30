6789b5cefee3dbf60947905e4a79a7e22c830f80
Reid McIlroy-Young <reid@reidmcy.com>
2015-06-30 13:25:20 -0400

    Moved blondel to main package and addded it to the testing suit

--- isilib/isilib/__init__.py
+++ isilib/isilib/__init__.py
@@ -4,3 +4,4 @@ from .recordCollection import RecordCollection, isiParser
 from .basicTest import btest
 from .graphHelpers import write_edgeList, write_nodeAttributeFile, write_graph, read_graph, ProgressBar, drop_edges, drop_nodesByDegree, drop_nodesByCount
 from .constants import VERBOSE_MODE, tagToFull, fullToTag, tagNameConverter, tagsAndNames
+from .blondel import blondel, modularity

65889a9a5a4ee62090113b25ae75936953a74f1c
Reid McIlroy-Young <reid@reidmcy.com>
2015-07-09 18:06:38 -0400

    Auto run should be working

--- vagrant/profile_wosserver/ipython_config.py
+++ vagrant/profile_wosserver/ipython_config.py
@@ -6,6 +6,7 @@ c.IPKernelApp.pylab = 'inline'
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
+c.NotebookApp.notebook_dir = '/vagrant'
 
 
 #------------------------------------------------------------------------------

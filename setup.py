a8a6a882c0ada11e694a71a4d9bc82cfd59a7d44
Reid McIlroy-Young <reid@reidmcy.com>
2015-07-17 00:01:23 -0400

    Updated version number to match website

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='isilib',
-    version='0.2',
+    version='0.1.0',
     author="Reid McIlroy-Young",
     url="https://github.com/mclevey/web_of_science_isi",
     keywords= 'isi wos',

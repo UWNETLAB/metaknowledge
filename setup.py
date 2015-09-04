45d3b1b6ea625068f8e3ac7dcc751f89d617bc92
Reid McIlroy-Young <reid@reidmcy.com>
2015-09-03 23:40:05 -0400

    Chnged stuff for PyPi test
    Former-commit-id: 6ec5b29d3fd035a7892b82913c08c92986acc4a2

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from metaknowledge.journalAbbreviations import abrevDBname
 
 from setuptools import setup, find_packages
 setup(name='metaknowledge',
-    version='0.2.0',
-    author="Reid McIlroy-Young",
+    version='0.3.0',
+    description = "A library for handling Web of science files",
+    author="Reid McIlroy-Young, John McLevey",
     author_email = "rmcilroy@uwaterloo.ca",
     url="https://github.com/networks-lab/metaknowledge",
+    download_url = "https://github.com/networks-lab/isilib/archive/0.3.tar.gz",
     keywords= 'isi wos testing',
+    classifiers = [],
     install_requires= ['networkx'],
     #extras_require={'visualizer' : ['matplotlib']},
     packages=find_packages(),#['metaknowledge', 'metaknowledge.journalAbbreviations'],

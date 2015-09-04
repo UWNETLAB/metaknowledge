82019871a41733748d368864dab78075a4de26d0
Reid McIlroy-Young <reid@reidmcy.com>
2015-09-04 00:03:23 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 setup(name='metaknowledge',
-    version='0.3.0',
+    version='0.3.1',
     description = "A library for handling Web of science files",
     author="Reid McIlroy-Young, John McLevey",
     author_email = "rmcilroy@uwaterloo.ca",
     url="https://github.com/networks-lab/metaknowledge",
-    download_url = "https://github.com/networks-lab/isilib/archive/0.3.tar.gz",
+    download_url = "https://github.com/networks-lab/isilib/archive/0.3.1.tar.gz",
     keywords= 'isi wos testing',
     classifiers = [],
     install_requires= ['networkx'],

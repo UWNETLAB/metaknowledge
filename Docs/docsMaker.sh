d806a7b37bef8cd4ec93594810a2bb43ffc02d1b
Reid McIlroy-Young <reid@reidmcy.com>
2015-09-07 14:02:11 -0400

    Added mdTonb and docs gen as scripts

--- Docs/docsMaker.sh
+++ Docs/docsMaker.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd "$(dirname "$0")"
-python3 docsMaker.py -o docs.md .
+python3 docsMaker.py -o docs.md
 pandoc --standalone --write=html --read=markdown --output=docs.html docs.md
 cp docs.md ../metaknowledgeDocs.md
 open docs.html

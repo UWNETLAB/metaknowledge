07a15110e0c76a9058a09adb3965ce7d40fe49c4
Reid McIlroy-Young <reid@reidmcy.com>
2015-09-10 03:24:25 -0400

    added autogen script

---
+++ documentation/MakeDocs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+echo "Generating docs and putting them in /_post/docs"
+echo "WARNING: /_post/docs is overwritten"
+cd "$(dirname "$0")"
+cd ../_posts/
+
+rm -r docs
+
+metaknowledge-DocsGen -d docs
+
+cd ..
+
+jekyll build

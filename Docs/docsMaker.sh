65fee6f0965e80c0ee0341998ee77af7ff7eb50d
Reid <reid@reidmcy.com>
2015-10-09 17:53:11 +0630

    Deleted supurflous directory and bumped version number
    Former-commit-id: b746abb1f313884eea47bd7e18e0b41d3888a49b

--- Docs/docsMaker.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-cd "$(dirname "$0")"
-python3 docsMaker.py -o docs.md
-pandoc --standalone --write=html --read=markdown --output=docs.html docs.md
-cp docs.md ../metaknowledgeDocs.md
-open docs.html

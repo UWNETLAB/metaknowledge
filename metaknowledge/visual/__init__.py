bb7fc3cdab78cbe9594d48c65cc32e4eb814354d
Reid <reid@reidmcy.com>
2015-12-21 00:34:32 +0530

    updated docs for visual and tagprocessing
    Former-commit-id: b693e5089ffaff4567943f061f9c135fb48b4f12

--- metaknowledge/visual/__init__.py
+++ metaknowledge/visual/__init__.py
@@ -1,4 +1,7 @@
 #Written by Reid McIlroy-Young for Dr. John McLevey, University of Waterloo 2015
-"""visual docstring"""
+"""Two functions based on _matplotlib_ for generating nicer looking graphs
+
+This is the only module that depends on anything besides _networkx_, as it depends on _numpy_, _scipy_ and _matplotlib_ in addition to _networkx_.
+"""
 
 from .plotting import graphDensityContourPlot, quickVisual

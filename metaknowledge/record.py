909c0791a8edd71bc4e370390cf0fecec31329dc
Reid <reid@reidmcy.com>
2016-01-20 02:10:50 +0530

    saving to do git stuff this commit does not work
    Former-commit-id: f00a4abfd60467b077aed4b9ef122d910a176896

--- metaknowledge/record.py
+++ metaknowledge/record.py
@@ -6,3 +6,15 @@ class Record(object):
         self.error = error
         self._id = idValue
         self._fieldDict = fieldDict
+
+    def __getitem__(self, key):
+        try:
+            return self._fieldDict[key]
+        except KeyError:
+            if isinstance(key, str):
+                KeyError("'{}' could not be found in the Record".format(key))
+            else:
+                raise TypeError("Keys to Records cannot be of the type: {}".format(type(key)))
+
+
+    def getTag(tag, raw = False):
